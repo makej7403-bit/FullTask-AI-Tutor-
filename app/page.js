b1844325386f85647c4fb905656ef3ba9e95eda0
makej7403-bit <makej7403@gmail.com>
2025-11-16 17:55:08 +0000

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import ChatUI from "../components/ChatUI";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <ChatUI />
+    </main>
+  );
+}
